1ed968f6238989c18a5d4c89227b26c433ade383
MarkAnthonySilva <masilva1619@gmail.com>
2020-05-14 23:03:37 -0700

    Added more get methods

--- Project/src/clinicalInformationSystem/model/PatientList.java
+++ Project/src/clinicalInformationSystem/model/PatientList.java
@@ -39,9 +39,22 @@ public class PatientList
 		return patientList.get(name);
 	}
 	
+	/**
+	 * Get an array representation of the patientList
+	 * @return an array representation of the patientList
+	 */
 	public PatientModel[] getPatientArray()
 	{
 		Collection<PatientModel> values = patientList.values();
 		return values.toArray(new PatientModel[values.size()]);
+	}
+	
+	/**
+	 * Get a tree map representation of this patient list
+	 * @return a tree map represenation of this patient list
+	 */
+	public TreeMap<String, PatientModel> getMap()
+	{
+		return this.patientList;
 	}
 }
