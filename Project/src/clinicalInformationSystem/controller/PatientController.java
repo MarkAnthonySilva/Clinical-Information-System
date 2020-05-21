dad16120e6853436211bf6996ed472d5df56b770
MarkAnthonySilva <masilva1619@gmail.com>
2020-05-21 15:28:39 -0700

    Fixed error where work status and education not saved

--- Project/src/clinicalInformationSystem/controller/PatientController.java
+++ Project/src/clinicalInformationSystem/controller/PatientController.java
@@ -148,8 +148,8 @@ public class PatientController implements ActionListener
 				
 				//Optional parameters
 				patient.setOccupation(patientData.get("Occupation"));
-				patient.setWorking(patientData.get("Work Staus"));
-				patient.setEducation(patientData.get("EducationDegree"));
+				patient.setWorking(patientData.get("Work Status"));
+				patient.setEducation(patientData.get("Educational Degree"));
 				patient.setNotes(patientData.get("Notes"));
 				
 				frame.getPatientList().addPatient(patientData.get("Name*"), patient);
